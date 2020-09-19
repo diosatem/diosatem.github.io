b44e89a3ba5072f982728167d339830d760790ba
diosatem <jam19021@byui.edu>
2020-09-19 08:50:29 +0800

    img

--- currentdate.js
+++ currentdate.js
@@ -1,6 +1,5 @@
 const dayNames = [
-    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-];
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 const months = [
     "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
