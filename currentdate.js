7d36737bf82a743bccc579ae967b4d9f87acd1d5
diosatem <jam19021@byui.edu>
2020-09-20 09:19:34 +0800

    updates

--- currentdate.js
+++ currentdate.js
@@ -3,10 +3,10 @@ const dayNames = [
 ];
 
 const months = [
-"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
 ];
 
-]
+
 const todaysdate = new Date();
 const dayName = dayNames[todaysdate.getDay()];
 const monthName = months[todaysdate.getMonth()];
