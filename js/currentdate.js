c220df49c9037c8137bc44edfc37c744b68fdc08
diosatem <jam19021@byui.edu>
2020-09-27 04:23:55 +0800

    updates

--- js/currentdate.js
+++ js/currentdate.js
@@ -14,4 +14,3 @@ const year = todaysdate.getFullYear();
 const currentdate = dayName + ", " + todaysdate.getDate() + monthName + ", " + todaysdate.getFullYear();
 
 document.getElementById('currentdate').textContent = currentdate;
-document.getElementById('currentYear').textContent = year;
