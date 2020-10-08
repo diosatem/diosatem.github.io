4e0ffc5abb55877051e771c04f478fd258345351
diosatem <jam19021@byui.edu>
2020-10-09 01:47:12 +0800

    updated

--- lesson3/js/lastmodified.js
+++ lesson3/js/lastmodified.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 let updated = document.lastModified;
-=======
-let updated = document.lastModified;
->>>>>>> f13efbfa150f223e968d93036d542e92e5fb5f8c
 document.getElementById("currentDate").innerHTML = updated;
