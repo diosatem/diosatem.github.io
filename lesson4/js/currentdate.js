cd25b1740ae25db5c02f0020d79072ca9eaf4228
diosatem <jam19021@byui.edu>
2020-10-10 08:25:38 +0800

    updates

--- lesson4/js/currentdate.js
+++ lesson4/js/currentdate.js
@@ -1,16 +1,2 @@
-const dayNames = [
-    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-];
-
-const months = [
-    "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-];
-
-
-const todaysdate = new Date();
-const dayName = dayNames[todaysdate.getDay()];
-const monthName = months[todaysdate.getMonth()];
-const year = todaysdate.getFullYear();
-const currentdate = dayName + ", " + todaysdate.getDate() + monthName + ", " + todaysdate.getFullYear();
-
-document.getElementById('currentdate').textContent = currentdate;
+var date = new Date("2015-03");
+document.getElementById("currentDate").innerText = date.toDateString();
