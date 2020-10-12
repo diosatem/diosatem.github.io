adb33c1222a2faec51ac7f19b914ca52e23e395c
diosatem <jam19021@byui.edu>
2020-10-13 03:06:55 +0800

    work updates

--- lesson4/js/currentdate.js
+++ lesson4/js/currentdate.js
@@ -1,2 +1,10 @@
-var date = new Date("2015-03");
-document.getElementById("currentDate").innerText = date.toDateString();
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
+const date = new Date().getFullYear();
+const d = new Date();
+const monthDay = d.getDate();
+
+document.getElementById('currentDate').innerHTML = days[d.getDay()] + ', ' + monthDay + ' ' + monthNames[d.getMonth()] + ' ' + date;
---
+++ lesson4/js/currentyear.js
@@ -0,0 +1,3 @@
+let date = new Date();
+let year = date.getFullYear();
+document.getElementById("currentYear").innerHTML = year;
