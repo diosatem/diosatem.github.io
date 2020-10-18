4d81966f2d364001a14032d2cae9966527fcabbd
diosatem <jam19021@byui.edu>
2020-10-18 12:05:46 +0800

    updates

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -1,3 +1,15 @@
+//current date
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
+const date = new Date().getFullYear();
+const d = new Date();
+const monthDay = d.getDate();
+
+document.getElementById('currentDate').innerHTML = days[d.getDay()] + ', ' + monthDay + ' ' + monthNames[d.getMonth()] + ' ' + date;
+
 //toggle
 function toggleMenu() {
     document
@@ -5,9 +17,8 @@ function toggleMenu() {
 }
 
 //site-wide notice
-var d = new Date();
-var x = d.getDay();
-
+var daydate = new Date();
+var x = daydate.getDay();
 
 if (x == 5) {
     document.getElementById("notice").style.display = "block";
