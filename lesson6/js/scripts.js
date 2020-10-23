2357b0031ba8ac4dca6adfb64755442a030c81b5
diosatem <jam19021@byui.edu>
2020-10-23 08:00:30 +0800

    updates

---
+++ lesson6/js/scripts.js
@@ -0,0 +1,39 @@
+//current date
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
+const date = new Date().getFullYear();
+const d = new Date();
+const monthDay = d.getDate();
+
+document.getElementById('currentDate').innerHTML = days[d.getDay()] + ', ' + monthDay + ' ' + monthNames[d.getMonth()] + ' ' + date;
+
+//toggle
+function toggleMenu() {
+    document
+        .getElementById("primaryNav").classList.toggle("hide");
+}
+
+//site-wide notice
+var daydate = new Date();
+var x = daydate.getDay();
+
+if (x == 5) {
+    document.getElementById("notice").style.display = "block";
+
+} else {
+    document.getElementById("notice").style.display = "none";
+    document.getElementsByTagName("header")[0].style.marginTop = "0";
+}
+
+//web font loader
+WebFont.load({
+    google: {
+        families: [
+            'Pacific'
+        ]
+    }
+});
+
