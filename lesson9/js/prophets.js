4ec44a149c7488f9ed8ed08f49440e2535bfb156
diosatem <jam19021@byui.edu>
2020-11-12 01:52:39 +0800

    updates

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -5,7 +5,7 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
-        console.table(jsonObject); // temporary checking for valid response and data parsing
+        console.table(jsonObject);
 
         const prophets = jsonObject['prophets'];
         for (let i = 0; i < prophets.length; i++) {
