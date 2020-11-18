c667fba64cc3766c4785396a9cedabe4d72d5462
diosatem <jam19021@byui.edu>
2020-11-18 23:01:02 +0800

    updates

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,15 +1,11 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=78e17b7d76d50d7fdea902505c1a1377';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=78e17b7d76d50d7fdea902505c1a1377';
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
 
-
         let valNum = jsObject.main.temp;
         document.getElementById('current-temp').textContent = (((valNum - 273.15) * 1.8) + 32).toFixed(2);
-
-
-
 
         const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
         const desc = jsObject.weather[0].description; // note how we reference the weather array
