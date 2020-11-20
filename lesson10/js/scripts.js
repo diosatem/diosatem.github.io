edffea1db1565bbcd03a4e2b991153b820d65ab9
diosatem <jam19021@byui.edu>
2020-11-20 10:14:53 +0800

    initial commit preston 10

--- lesson10/js/scripts.js
+++ lesson10/js/scripts.js
@@ -85,48 +85,6 @@ if ('IntersectionObserver' in window) {
     });
 }
 
-//town data for index.html
-const reqURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
-fetch(reqURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        const towns = jsonObject['towns'];
-
-        for (let i = 0; i < towns.length; i++) {
-            if (towns[i].name == "Fish Haven" || towns[i].name == "Preston" || towns[i].name == "Soda Springs") {
-                let box = document.createElement('section');
-
-                let title = document.createElement('h2');
-                title.textContent = towns[i].name;
-                box.appendChild(title);
-
-                let h3 = document.createElement('h3');
-                h3.textContent = towns[i].motto;
-                box.appendChild(h3);
-
-                let year = document.createElement('p');
-                year.textContent = 'Year Founded:' + ' ' + towns[i].yearFounded;
-                box.appendChild(year);
-
-                let pop = document.createElement('p');
-                pop.textContent = 'Population:' + ' ' + towns[i].currentPopulation;
-                box.appendChild(pop);
-
-                let rainfall = document.createElement('p');
-                rainfall.textContent = 'Annual Rainfall:' + ' ' + towns[i].averageRainfall;
-                box.appendChild(rainfall);
-
-                let image = document.createElement('img');
-                image.setAttribute('src', '/lesson9/images/' + towns[i].photo);
-                image.alt = towns[i].name;
-                box.appendChild(image);
-
-                document.querySelector('div.towndata').appendChild(box);
-            }
-        }
-    });
 
     
--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -15,20 +15,55 @@ fetch(apiURL)
         document.getElementById('icon').setAttribute('alt', desc);
     });
 
-    //weather summary for preston page
-    const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=78e17b7d76d50d7fdea902505c1a1377';
+//weather summary for preston page
+const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=78e17b7d76d50d7fdea902505c1a1377';
 fetch(weatherURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
 
-        let weatherDesc = jsObject.weather[0].description;
+        let weatherDesc = jsObject.weather[0].main;
         document.getElementById('currentWeather').textContent = weatherDesc;
 
         let valNum = jsObject.main.temp_max;
         document.getElementById('high').textContent = (((valNum - 273.15) * 1.8) + 32).toFixed(2);
 
+        let humid = jsObject.main.humidity;
+        document.getElementById('humidity').textContent = humid;
+
+        let windS = jsObject.wind.speed;
+        document.getElementById('windSpeed').textContent = windS;
+    });
+
+//5-day forecast for preston page
+const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=78e17b7d76d50d7fdea902505c1a1377';
+fetch(forecastURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        for (let i = 0; i <= 5; i++) {
+            let day = jsObject.list[0].dt;
+            document.getElementById('dayId').textContent = day;
+
+            const iconSrc = 'https://openweathermap.org/img/w/' + jsObject.list[0].weather[0].icon + '.png'; // note the concatenation
+        const desc = jsObject.list[0].weather[0].description;
+        document.getElementById('dayIcon').setAttribute('src', iconSrc);
+        document.getElementById('dayIcon').setAttribute('alt', desc);
+
+
+        }
+
 
 
         
+
+
+
+        
+
+        let day1temp = jsObject.list[0].main.temp;
+        document.getElementById('dayOnetemp').textContent = day1temp;
+
+
     });
