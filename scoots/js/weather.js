bc75950bb57463842d894690ed8fcfecfea41fcb
diosatem <jam19021@byui.edu>
2020-12-15 01:36:17 +0800

    updates

--- scoots/js/weather.js
+++ scoots/js/weather.js
@@ -1,4 +1,4 @@
-//weather summary for fish haven page
+//weather summary
 const reqURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=20.5072&lon=-86.9446&exclude=hourly,daily&units=imperial&appid=78e17b7d76d50d7fdea902505c1a1377';
 fetch(reqURL)
     .then((response) => response.json())
@@ -15,7 +15,6 @@ fetch(reqURL)
         document.getElementById('temperature').textContent = temp;
 
         const desc = jsObject.current.weather[0].main;
-        console.log(desc);
         document.getElementById('description').textContent = desc;
 
         const humid = jsObject.current.humidity;
@@ -25,21 +24,27 @@ fetch(reqURL)
 //3-day forecast
 const forecastURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=20.5072&lon=-86.9446&units=imperial&appid=78e17b7d76d50d7fdea902505c1a1377';
 fetch(forecastURL)
-    .then((response) => response.json())
-    .then((jsObject) => {
-        console.log(forecastURL);
-        const forecast = jsObject.daily.filter(fcast => fcast.temp.includes('day:'));
-        console.log(forecast);
-        const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    .then((res) => res.json())
+    .then(res => {
+        const {
+            daily
+        } = res;
 
-        for (let day = 0; day < forecast.length; day++) {
-            const d = new Date(forecast[day].temp);
+        // const tempDayOne = daily[0]['temp']['day'];
+        // const tempDayTwo = daily[1]['temp']['day'];
+        // const tempDayThree = daily[2]['temp']['day'];
+        // console.log(tempDayOne)
+        // console.log(tempDayTwo)
+        // console.log(tempDayThree)
+
+        const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        for (let day = 0; day < daily.length; day++) {
+            const d = new Date(daily[day].temp.day);
             console.log(d);
             document.getElementById(`dayId${day+1}`).textContent = weekdays[d.getDay()];
-            document.getElementById(`dayTemp${day+1}`).textContent = forecast[day].temp.day;
-
-            const imgSrc = 'https://openweathermap.org/img/w/' + forecast[day].weather[0].icon + '.png';
-            const altDesc = forecast[day].weather[0].description;
+            document.getElementById(`dayTemp${day+1}`).textContent = daily[day].temp.day;
+            const imgSrc = 'https://openweathermap.org/img/w/' + daily[day].weather[0].icon + '.png';
+            const altDesc = daily[day].weather[0].description;
             document.getElementById(`ficon${day+1}`).setAttribute('src', imgSrc);
             document.getElementById(`ficon${day+1}`).setAttribute('alt', altDesc);
         }
@@ -51,10 +56,7 @@ fetch(forecastURL)
         const alertText = jsObject.alert.description;
         document.getElementById('alertdesc').textContent = alertText;
         console.log(alertText);
-            
     });
-
-
 //collapsible alerts
 var coll = document.getElementsByClassName("collapsible");
 var i;
