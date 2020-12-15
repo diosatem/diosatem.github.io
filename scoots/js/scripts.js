2474e27ef4180065cb8eb15bf48047071cf85b40
diosatem <jam19021@byui.edu>
2020-12-15 14:48:08 +0800

    updates

--- scoots/js/scripts.js
+++ scoots/js/scripts.js
@@ -23,6 +23,17 @@ WebFont.load({
     }
 });
 
+//last modified date
+let updated = document.lastModified;
+document.getElementById("lastmod").innerHTML = updated;
+
+//current year
+let dates = new Date();
+let year = dates.getFullYear();
+document.getElementById("currentYear").innerHTML = year;
+
+
+
 
 
 
